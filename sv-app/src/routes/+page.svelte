3d4cde56554e218864dc1a36036ad8ad3a91ae4e
Ankur Seth <iankurseth@gmail.com>
2025-04-04 10:42:52 +0530

    Added some text styles

--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<section class="flex items-center justify-center min-h-screen">
+	<h1 class="">Welcome to Localprompt</h1>
+</section>
